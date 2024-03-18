443cbc1f5b77e092ac7bdd06643c6232de5eb2e1
OmarAzizi <oalazizi75@gmail.com>
2024-03-18 22:42:39 +0300

    Main function of the compiler

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "chunk.h"
+#include "common.h"
+#include "debug.h"
+
+int main(int argc, char** argv) {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+  
+    disassembleChunk(&chunk, "test chunk");
+
+    freeChunk(&chunk);
+    return 0;
+} 
