3e85cedf6815e5c3d7e637d047c6e531b870cdfd
OmarAzizi <oalazizi75@gmail.com>
2024-03-18 23:37:22 +0300

    Added constant pooling to Bytecode chunks

--- chunk.c
+++ chunk.c
@@ -2,10 +2,11 @@
 #include "chunk.h"
 #include "memory.h"
 
-void initChunk(Chunk *chunk) {
+void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    initValueArray(&chunk->constants);
 }
 
 /*
@@ -21,7 +22,13 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
     chunk->code[chunk->count++] = byte;
 }
 
-void freeChunk(Chunk *chunk) {
+void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
+}
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+    return chunk->constants.count - 1; // return the undex where the constant was appedned so we can locate it later
 }
--- chunk.h
+++ chunk.h
@@ -8,6 +8,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 /*
     each instruction has a one-byte operation code 
@@ -25,10 +26,14 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+
+/* This is a convinence method to add a new constant to the chunk */
+int addConstant(Chunk* chunk, Value value);
 
 #endif
