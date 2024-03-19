d63ce2453ae05046a80378df8f9fb21eb8004ac7
OmarAzizi <oalazizi75@gmail.com>
2024-03-19 23:12:14 +0300

    Testing the added stuff

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 int main(int argc, char** argv) {
     Chunk chunk;
     initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2); // will add a constant to the values array of chunk and return its index
+    writeChunk(&chunk, OP_CONSTANT);
+    writeChunk(&chunk, constant); // writing the index of the constant to the chunk
+
     writeChunk(&chunk, OP_RETURN);
   
     disassembleChunk(&chunk, "test chunk");
