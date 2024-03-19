d24a3631f191b61ff6c73eaed2ff66f0c277dcc9
OmarAzizi <oalazizi75@gmail.com>
2024-03-19 23:28:25 +0300

    Implementing the line information thing

--- chunk.c
+++ chunk.c
@@ -6,6 +6,7 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
@@ -13,17 +14,21 @@ void initChunk(Chunk* chunk) {
     The dynamic array starts off completely empty.  We don’t even allocate 
     a raw array yet. To append  a byte to the end of the chunk, we use a new function.
 */
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);        
     }
-    chunk->code[chunk->count++] = byte;
+    chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
+    ++chunk->count;
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
