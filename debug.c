5eb7382e7e67aab0015db718a5839d7776157bc8
OmarAzizi <oalazizi75@gmail.com>
2024-03-19 23:29:34 +0300

    Now the disassembler can show line numbers of instructions

--- debug.c
+++ debug.c
@@ -28,6 +28,10 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
 */
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset); // prints the byte offset of each instruction
+    
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
+        printf("    | "); // we show a '|' for any instruction that comes from the same source line as the preceding one
+    else printf("%4d ", chunk->lines[offset]);
 
     uint8_t instruction = chunk->code[offset]; // reads single byte from bytecode
     switch (instruction) {
