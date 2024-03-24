adfee8207f1d71f9e588795dab4560178fa271a3
OmarAzizi <oalazizi75@gmail.com>
2024-03-24 14:03:10 +0300

    Testing new features by evaluating an arithmetic expression

--- main.c
+++ main.c
@@ -12,8 +12,24 @@ int main(int argc, char** argv) {
     int constant = addConstant(&chunk, 1.2); // will add a constant to the values array of chunk and return its index
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123); // writing the index of the constant to the chunk
-    writeChunk(&chunk, OP_RETURN, 123);
-  
+
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123); 
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123); 
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    // compiled expression => -((1.2 + 3.4) / 5.6) = -0.821429
+    writeChunk(&chunk, OP_RETURN, 123); // end of chunk
+
     interpret(&chunk); // telling the VM interpret a chunk of bytecode 
 
     freeVM();
