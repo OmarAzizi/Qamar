48e76bb6bc4a36b079804f4b1fe42ccaf68e76db
OmarAzizi <oalazizi75@gmail.com>
2024-03-24 13:59:34 +0300

    Added new 'binary' arithmetic instructions (OP_ADD, OP_SUBTRACT, OP_DIVIDE, OP_MULTIPLY)

--- chunk.h
+++ chunk.h
@@ -16,6 +16,10 @@
 */
 typedef enum {
     OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
     OP_NEGATE, // Unary negation (a = 12 | -a == -12)
     OP_RETURN, // return instruction
 } OpCode;
