1fb356ee20c782ed744754ac6b2f78a1818ef84c
OmarAzizi <oalazizi75@gmail.com>
2024-03-26 09:17:24 +0300

    Removed hardcoded chunks of Bytecode and replaced it with some functions that would read actual source and get the compiler going

--- main.c
+++ main.c
@@ -1,38 +1,71 @@
-#include "chunk.h"
-#include "common.h"
-#include "debug.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "vm.h"
+#include "common.h"
+
+static void repl() {
+    char line[1024];
+    while (true) {
+        printf("> ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        interpret(line);
+    }
+}
+
+static char* readFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit (74);
+    }
+     
+    /* Figuring out the size of the file (in Bytes) */
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+    
+    /* Allocating memory for the file buffer */
+    char* buffer = (char*)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+    buffer[bytesRead + 1] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
 
 int main(int argc, char** argv) {
     initVM();
-
-    Chunk chunk;
-    initChunk(&chunk);
-
-    int constant = addConstant(&chunk, 1.2); // will add a constant to the values array of chunk and return its index
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123); // writing the index of the constant to the chunk
-
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123); 
-
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123); 
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-    // compiled expression => -((1.2 + 3.4) / 5.6) = -0.821429
-    writeChunk(&chunk, OP_RETURN, 123); // end of chunk
-
-    interpret(&chunk); // telling the VM interpret a chunk of bytecode 
+    
+    if (argc == 1) repl(); // Read, Evaluate, Print, Loop
+    else if (argc == 2) runFile(argv[1]); // Read source file
+    else {
+        fprintf(stderr, "Usage: clox [path]\n");
+        exit(64);
+    }
 
     freeVM();
-    freeChunk(&chunk);
     return 0;
 } 
