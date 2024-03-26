9c9c0a4790630d577ebb86e914450cc0556ad945
OmarAzizi <oalazizi75@gmail.com>
2024-03-26 10:35:14 +0300

    Added most of the needed functionalities for the lexer

--- scanner.c
+++ scanner.c
@@ -18,6 +18,8 @@ void initScanner(const char* source) {
     scanner.line = 1;
 }
 
+static bool isDigit(char c) { return c >= '0' && c <= '9'; }
+
 static bool isAtEnd() { return *scanner.current == '\0'; }
 
 /* This is a Token constructor */
@@ -30,6 +32,31 @@ static Token makeToken(TokenType type) {
     return token;
 }
 
+static char advance() {
+    ++scanner.current;
+    return scanner.current[-1];
+}
+
+/* This returns the current charchter without consuming it */
+static char peek() { return *scanner.current; }
+
+static char peekNext() {
+    if (isAtEnd()) return '\0';
+    return scanner.current[1];
+}
+
+/*
+    If the current character is the desired one, we advance and return true. 
+    Otherwise, we return false to indicate it wasn’t matched.
+
+*/
+static bool match(char expected) {
+    if (isAtEnd()) return false;
+    if (*scanner.current != expected) return false;
+    ++scanner.current;
+    return true;
+}
+
 static Token errorToken(const char* message) {
     Token token;
     token.type = TOKEN_ERROR;
@@ -40,10 +67,100 @@ static Token errorToken(const char* message) {
 }
 
 /*
+    This advances the scanner past any leading whitespace. After this call 
+    returns, we know the very next character is a meaningful one
+*/
+static void skipWhitespace() {
+    while (true) {
+        char c = peek();
+        switch (c) {
+            case ' ':
+            case '\r':
+            case '\t':
+                advance();
+                break;
+            case '\n':
+                ++scanner.line;
+                advance();
+                break;
+            case '/': /* skipping comments */
+                if (peekNext() == '/') {
+                    // A comment does untill the end of the line
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else return;
+                break;
+            default: return;
+        }
+    }
+}
+
+static Token number() {
+    while (isDigit(peek())) advance();
+
+    /* Look for a fractional part */
+    if (peek() == '.' && isDigit(peekNext())) {
+        /* Consume the "." */
+        advance();
+        while (isDigit(peek())) advance();
+    }
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') ++scanner.line; /* Tracking lines (supporting multi-line strings) */
+        advance();
+    }
+
+    if (isAtEnd()) return errorToken("Unterminated string.");
+
+    // The closing quote
+    advance();
+    return makeToken(TOKEN_STRING);
+}
+
+/*
     Each call to 'scanToken' scans a complete token
 */
 Token scanToken() {
+    skipWhitespace();
     scanner.start = scanner.current;
     if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+    char c = advance();
+
+    if (isDigit(c)) return number();
+    
+    switch (c) {
+        case '(': return makeToken(TOKEN_LEFT_PAREN);
+        case ')': return makeToken(TOKEN_RIGHT_PAREN);
+        case '{': return makeToken(TOKEN_LEFT_BRACE);
+        case '}': return makeToken(TOKEN_RIGHT_BRACE);
+        case ';': return makeToken(TOKEN_SEMICOLON);
+        case ',': return makeToken(TOKEN_COMMA);
+        case '.': return makeToken(TOKEN_DOT);
+        case '-': return makeToken(TOKEN_MINUS);
+        case '+': return makeToken(TOKEN_PLUS);
+        case '/': return makeToken(TOKEN_SLASH);
+        case '*': return makeToken(TOKEN_STAR);
+        case '!': 
+            return makeToken(
+                match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG
+            );
+        case '=':
+            return makeToken(
+                match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL
+            );
+        case '<':
+            return makeToken(
+                match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS        
+            );
+        case '>':
+            return makeToken(
+                match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER
+            );
+        case '"': return string();
+    }
+
     return errorToken("Unexpected charchter.");
 }
