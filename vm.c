275afe3938baf1ca8a7e04fe9dfd052f4d67860f
OmarAzizi <oalazizi75@gmail.com>
2024-03-28 13:34:20 +0300

    Interpreting the `OP_NOT` instruction generated by the compiler in the VM

--- vm.c
+++ vm.c
@@ -47,6 +47,13 @@ static Value peek(int distance) {
     return vm.stackTop[-1 - distance];
 }
 
+/*
+    Here i'm following the rule in Ruby that `nil` and `false` are falsey and every other value behaves like `true`
+*/
+static bool isFalsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++) // This macro reads the byte currently pointed at by the instruction pointer and then it increments it
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
@@ -93,6 +100,7 @@ static InterpretResult run() {
             case OP_SUBTRACT:   BINARY_OP(NUMBER_VAL, -); break;
             case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
             case OP_DIVIDE:     BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NOT:        push(BOOL_VAL(isFalsey(pop()))); break;
             case OP_NEGATE:     
                 if (!IS_NUMBER(peek(0))) {
                     runtimeError("Operand must be a number.");
