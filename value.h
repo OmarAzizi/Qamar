f9a54891f4f394ea46fa0f0c5542e447dd8a437b
OmarAzizi <oalazizi75@gmail.com>
2024-03-28 12:23:57 +0300

    Changed the  typdef to implement the language datatypes (bool, nil, number) using a tagged union with some helper macros to convert back and forth between  and C Datatype

--- value.h
+++ value.h
@@ -1,16 +1,32 @@
-/*
-    This module basically implements something similar to (constant pool) in Java.
-*/
-
 #ifndef clox_value_h
 #define clox_value_h
 
 #include "common.h"
 
-/*
-    This typedef abstracts how Lox values are concretely represented in C
-*/
-typedef double Value;
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL, 
+    VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+    ValueType type;
+    union {
+        bool boolean;
+        double number;
+    } as; 
+} Value;
+
+#define BOOL_VAL(value)   ((Value) {VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value) {VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value) {VAL_NUMBER, {.number = value}})
+
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
 
 /* The implemntation for the following is really similar to Chunk implemntation */
 typedef struct {
