4463f2de2277a98dbe290c344c13763a64ca26c2
OmarAzizi <oalazizi75@gmail.com>
2024-03-28 13:14:42 +0300

    Handling the new datatypes in  function

--- value.c
+++ value.c
@@ -24,4 +24,10 @@ void freeValueArray(ValueArray* array) {
     initValueArray(array);
 }
 
-void printValue(Value value) { printf("%g", AS_NUMBER(value)); }
+void printValue(Value value) { 
+    switch (value.type) {
+        case VAL_BOOL:   printf(AS_BOOL(value) ? "true" : "false"); break;
+        case VAL_NIL:    printf("nil"); break;
+        case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    }
+}
