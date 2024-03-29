5270da030a37e22b49a4f484e2b3c0bac13c16ff
OmarAzizi <oalazizi75@gmail.com>
2024-03-29 13:01:24 +0300

    Made a new module for heap-allocated objects

---
+++ object.h
@@ -0,0 +1,45 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+/*
+    This module implements all heap-allocated object 
+    in the language such as strings, instances, functions, etc...
+*/
+
+#include "common.h"
+#include "value.h"
+
+/* This macro that extracts the object type tag from a given Value. */
+#define OBJ_TYPE(value)  (AS_OBJ(value)->type)
+
+/* When we cast an `Obj*` to `ObjString*` we need to make sure it points to an `obj` field of an actial `ObjString` */
+#define IS_STRING(value)  isObjType(value, OBJ_STRING)
+
+/*
+    These two macros take a Value that is expected to contain a pointer to a valid ObjString on the heap. 
+    The first one returns the ObjString* pointer. The second one steps through that to return the character array itself.
+*/
+#define AS_STRING(value)  ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
+
+typedef enum {
+    OBJ_STRING,
+} ObjType;
+
+struct Obj {
+    ObjType type;
+};
+
+struct ObjString {
+    Obj obj;
+    int length;
+    char* chars;
+};
+
+ObjString* copyString(const char* chars, int length);
+
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
