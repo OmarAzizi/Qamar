d37ccca9f2443e011a7e320fb1e1ca4424f3dc19
OmarAzizi <oalazizi75@gmail.com>
2024-03-29 13:09:49 +0300

    Added a new function that would handle printing the heap-allocated objects in VM

--- object.c
+++ object.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <string.h>
 
 #include "memory.h"
@@ -25,4 +26,10 @@ ObjString* copyString(const char* chars, int length) {
     memcpy(heapChars, chars, length);
     heapChars[length] = '\0';
     return allocateString(heapChars, length);
+}
+
+void printObject(Value value) {
+    switch (OBJ_TYPE(value)) {
+        case OBJ_STRING: printf("%s", AS_CSTRING(value)); break;
+    }
 }
