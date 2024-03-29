bcc03ba1c1ee256d47f09dc87b5f1b43b6ec993e
OmarAzizi <oalazizi75@gmail.com>
2024-03-29 14:03:28 +0300

    Adding new functions and helper macros to help with cleaning up the memory to avoid memory leaks

--- memory.c
+++ memory.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "memory.h"
+#include "value.h"
+#include "vm.h"
 
 /*
     This function is the single function we will use for all dynamic memory menagement in clogc 
@@ -22,4 +24,24 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
     void* result = realloc(pointer, newSize);
     if (result == NULL) exit(1);
     return result;
+}
+
+static void freeObject(Obj* object) {
+    switch (object->type) {
+        case OBJ_STRING: {
+            ObjString* string = (ObjString*)object;
+            FREE_ARRAY(char, string->chars, string->length + 1);
+            FREE(ObjString, object);
+            break;
+        }
+    }
+}
+
+void freeObjects() {
+    Obj* object = vm.objects;
+    while (object != NULL) {
+        Obj* next = object->next;
+        freeObject(object);
+        object = next;
+    }
 }
