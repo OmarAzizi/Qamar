8d2b2ab08f982601fd0c25b022bc88e7129c5be5
OmarAzizi <oalazizi75@gmail.com>
2024-04-01 16:06:31 +0300

    Finished the hash table implementation

--- table.h
+++ table.h
@@ -20,11 +20,27 @@ typedef struct {
 void initTable(Table* table);
 void freeTable(Table* table);
 
+/*
+    For this function you pass in a table and a key. If it finds an entry with that key, it returns true, otherwise it returns false. 
+    If the entry exists, the value output parameter points to the resulting value.
+*/
+bool tableGet(Table* table, ObjString* key, Value* value);
+
 /* 
     This function adds the given key/value pair to the given hash table 
-
     return -> true if the new entry was added
 */
 bool tableSet(Table* table, ObjString* key, Value value);
+
+/*
+    This function deletes an entry from the table
+*/
+bool tableDelete(Table* table, ObjString* key);
+
+/*
+    This is a helper method that copies all of the entries of on hach table to another
+*/
+void tableAddAll(Table* from, Table* to);
+ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash);
 
 #endif
