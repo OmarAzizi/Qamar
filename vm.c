43963c8b0e5ffa747a8a6c9d2133a29b6469b31d
OmarAzizi <oalazizi75@gmail.com>
2024-04-02 20:03:52 +0300

    Added new case int the interpreter to support `OP_PRINT`

--- vm.c
+++ vm.c
@@ -150,9 +150,13 @@ static InterpretResult run() {
                 }
                 push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
-            case OP_RETURN: {
+            case OP_PRINT: {
                 printValue(pop());
                 printf("\n");
+                break;
+            }
+            case OP_RETURN: {
+                /* Exit interpreter */
                 return INTERPRET_OK;
             }
         }
