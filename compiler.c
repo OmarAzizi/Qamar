7ab09842812b0316f5c5fe6bf7536da14082a852
OmarAzizi <oalazizi75@gmail.com>
2024-04-03 17:34:24 +0300

    Adding variables retrieval from the hash table. So now we can refer to the variables using their names

--- compiler.c
+++ compiler.c
@@ -139,10 +139,10 @@ static void endCompiler() {
 }
 
 static void expression();
-static ParseRule* getRule(TokenType type);
-static void parsePrecedence(Precedence precedence);
 static void statement();
 static void decleration();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
 static uint8_t identifierConstant(Token* name);
 static uint8_t parseVariable(const char* errorMessage);
 static void defineVariable(uint8_t global);
@@ -282,6 +282,15 @@ static void string() {
     emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
+static void namedVariable(Token name) {
+    uint8_t arg = identifierConstant(&name);
+    emitBytes(OP_GET_GLOBAL, arg);
+}
+
+static void variable() {
+    namedVariable(parser.previous);
+}
+
 static void unary() {
     TokenType operatorType = parser.previous.type;
      
@@ -317,8 +326,8 @@ ParseRule rules[] = {
     [TOKEN_GREATER_EQUAL] = {NULL,      binary, PREC_COMPARISON},
     [TOKEN_LESS]          = {NULL,      binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL]    = {NULL,      binary, PREC_COMPARISON},
-    [TOKEN_IDENTIFIER]    = {NULL,      NULL,         PREC_NONE},
-    [TOKEN_STRING]        = {string,      NULL,       PREC_NONE},
+    [TOKEN_IDENTIFIER]    = {variable,  NULL,         PREC_NONE},
+    [TOKEN_STRING]        = {string,    NULL,         PREC_NONE},
     [TOKEN_NUMBER]        = {number,    NULL,         PREC_NONE},
     [TOKEN_AND]           = {NULL,      NULL,         PREC_NONE},
     [TOKEN_CLASS]         = {NULL,      NULL,         PREC_NONE},
