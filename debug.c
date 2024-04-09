f62af3c381ef3ebae5c86885e17db5d6f73ee44e
OmarAzizi <oalazizi75@gmail.com>
2024-04-09 22:32:20 +0300

    Adding disassembler support for new bytecode instructions `OP_JUMP` & `OP_JUMP_IF_FALSE`

--- debug.c
+++ debug.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
+#include "chunk.h"
 
 void disassembleChunk(Chunk* chunk, const char *name) {
     printf("== %s ==\n", name); 
@@ -16,6 +17,13 @@ static int byteInstruction(const char* name, Chunk* chunk, int offset) {
     uint8_t slot = chunk->code[offset + 1];
     printf("%-16s %4d\n", name, slot);
     return offset + 2;
+}
+
+static int jumpInstruction(const char* name, int sign, Chunk* chunk, int offset) {
+    uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
+    jump |= chunk->code[offset + 2];
+    printf("%-16s %4d -> %d\n", name, offset, offset + 3 + sign * jump);
+    return offset + 3;
 }
 
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
@@ -80,6 +88,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_NEGATE", offset);
         case OP_PRINT:
             return simpleInstruction("OP_PRINT", offset);
+        case OP_JUMP:
+            return jumpInstruction("OP_JUMP", 1, chunk, offset);
+        case OP_JUMP_IF_FALSE:
+            return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         default:
