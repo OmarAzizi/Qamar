ae469df270a1f3036f18ac537bc223f7eb59122d
OmarAzizi <oalazizi75@gmail.com>
2024-04-18 00:25:18 +0300

    Adding frontend support for `return` statements in the body of functions

--- compiler.c
+++ compiler.c
@@ -465,6 +465,20 @@ static void printStatement() {
     emitByte(OP_PRINT); 
 }
 
+static void returnStatement() {
+    if (current->type == TYPE_SCRIPT) {
+        error("Can't return from top-level code.");
+    }
+
+    if (match(TOKEN_SEMICOLON)) {
+        emitReturn();
+    } else {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+        emitByte(OP_RETURN);
+    }
+}
+
 static void whileStatement() {
     int loopStart = currentChunk()->count;
 
@@ -534,6 +548,8 @@ static void statement() {
         forStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
+    } else if (match(TOKEN_RETURN)) {
+        returnStatement();
     } else if (match(TOKEN_WHILE)) {
         whileStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
