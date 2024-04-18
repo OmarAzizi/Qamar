20ddb26a7ec430358fbe37fc353bf90618d6aec6
OmarAzizi <oalazizi75@gmail.com>
2024-04-18 14:50:53 +0300

    Handling the case where we want to free `OBJ_NATIVE` object in `freeObject` function

--- memory.c
+++ memory.c
@@ -37,6 +37,9 @@ static void freeObject(Obj* object) {
             FREE(OBJ_FUNCTION, object);
             break;
         }
+        case OBJ_NATIVE:    
+            FREE(ObjNative, object);
+            break;
         case OBJ_STRING: {
             ObjString* string = (ObjString*)object;
             FREE_ARRAY(char, string->chars, string->length + 1);
