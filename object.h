c07e53314ee84a42e0d0ae5e9f5b3c6238872cf4
OmarAzizi <oalazizi75@gmail.com>
2024-04-27 01:03:55 +0300

    fixed coding style

--- object.h
+++ object.h
@@ -97,8 +97,8 @@ ObjClosure*  newClosure(ObjFunction* function);
 ObjFunction* newFunction();
 ObjNative*   newNative(NativeFn function);
 
-ObjString* takeString(char* chars, int length);
-ObjString* copyString(const char* chars, int length);
+ObjString*  takeString(char* chars, int length);
+ObjString*  copyString(const char* chars, int length);
 ObjUpvalue* newUpvalue(Value* slot);
 void printObject(Value value);
 
